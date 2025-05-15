4d65c10f55f04ded56fd2727da8a3ded996e8cc6
yangdongeun <xdfs0214@naver.com>
2025-05-15 18:05:02 +0900

    main js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
-	$(".headC").click(function(){
+	$(".allMenu").click(function(){
 		$(".headB").slideToggle();
 	});
 });
 
 window.addEventListener('scroll', function () {
-	const header = document.getElementById('mainHeader');
+	const header = document.querySelector('.mainHeader');
 	if (window.scrollY > 10) {
-	  header.classList.add('scrolled');
+	  	header.classList.add('scrolled');
+		setTimeout(function () {
+			header.classList.remove('scrolled');
+		}, 1000);
 	} else {
-	  header.classList.remove('scrolled');
+		header.classList.remove('scrolled');
 	}
 });
