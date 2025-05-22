bd48c17954e3315bd9883da86fd5cdcd582b8ebd
yangdongeun <xdfs0214@gmail.com>
2025-05-22 21:49:49 +0900

    js 변경

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
-$(document).ready(function(){
+function checkAllMenuVisible() {
+	const headB = $(".headB");
+	const headD = $(".headD");
 
+	if (window.innerWidth > 968) {
+		headB.slideUp(300);
+		headD.fadeIn(200);
+	}
+}
+
+function changeLogin(loginstate) {
+	if (loginstate) {
+		$(".headD").html(`
+            <ul>
+				<li><a href="logout.html">로그아웃</a></li>
+                <li><a href="mypage.html">마이페이지</a></li>
+            </ul>
+        `);
+	} else {
+		$(".headD").html(`
+            <ul>
+                <li><a href="login.html">로그인</a></li>
+                <li><a href="signup.html">회원가입</a></li>
+            </ul>
+        `);
+	}
+}
+
+$(document).ready(function(){
+	
 	//모바일 화면용 버튼 구현
 	$(".allMenu").click(function(){
 		const headB = $(".headB");
@@ -13,16 +41,27 @@ $(document).ready(function(){
 			headD.fadeOut(200);
 		}
 	});
+	
+	$(window).on('scroll', function() {
+		const header = $(".mainHeader");
+		const headB = $(".headB");
+		const headD = $(".headD");
+
+		if ($(window).scrollTop() > 10) {
+			header.addClass("scrolled");
+			setTimeout(function () {
+				header.removeClass("scrolled");
+			}, 1000);
+		} else {
+			header.removeClass("scrolled");
+		}
+		headB.slideUp(300);
+		headD.fadeIn(200);
+	});
+
+	$(window).on('resize', function(){
+		checkAllMenuVisible();
+	});
+	checkAllMenuVisible();
 });
 
-window.addEventListener('scroll', function () {
-	const header = document.querySelector('.mainHeader');
-	if (window.scrollY > 10) {
-	  	header.classList.add('scrolled');
-		setTimeout(function () {
-			header.classList.remove('scrolled');
-		}, 1000);
-	} else {
-		header.classList.remove('scrolled');
-	}
-});
